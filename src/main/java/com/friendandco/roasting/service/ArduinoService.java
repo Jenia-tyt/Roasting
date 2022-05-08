ada69345c0a868458b2d9b7fa452e821cf846bfc
Evgeniy Uvarov <evgeniy.uvarov@gridnine.com>
2022-05-08 17:44:18 +0300

    Base done

--- src/main/java/com/friendandco/roasting/service/ArduinoService.java
+++ src/main/java/com/friendandco/roasting/service/ArduinoService.java
@@ -1,5 +1,6 @@
 package com.friendandco.roasting.service;
 
+import com.fazecast.jSerialComm.SerialPort;
 import com.friendandco.roasting.component.Translator;
 import com.friendandco.roasting.multiThread.ThreadPoolFix;
 import javafx.concurrent.Task;
@@ -7,6 +8,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
 
 @Slf4j
 @Getter
@@ -22,33 +25,71 @@ public class ArduinoService extends Task<Void> {
     private boolean connect = true;
 
     @Override
-    protected Void call() throws Exception {
+    protected Void call()  {
         //TODO реализовтаь
         // тут метод который уставливает конет
 
+        test();
+
+
         Double value = 0.0;
         while (true) {
-            if (connect) {
-//                if (infoService.isShowing()) {
-//                    infoService.clear();
-//                }
-                currentTemperature = value;
-                if (temperatureDrawService.isInitDone()) {
-                    temperatureDrawService.draw(currentTemperature);
+
+            try {
+
+
+                if (connect) {
+                    currentTemperature = value;
+                    if (temperatureDrawService.isInitDone()) {
+                        temperatureDrawService.draw(currentTemperature);
+                    }
+                    value = value + 1;
+                } else {
+                    //TODO
+                    // мы показываем попа
+                    // пытаемся установить конект снова
+                    if (!infoService.isShowing()) {
+                        infoService.showError(
+                                translator.getMessage("thermocouple.not_connect"),
+                                1000
+                        );
+                    }
                 }
-                value = value + 1;
-            } else {
-                //TODO
-                // мы показываем попа
-                // пытаемся установить конект снова
-                if (!infoService.isShowing()) {
-                    infoService.showError(
-                            translator.getMessage("thermocouple.not_connect"),
-                            1000
-                    );
-                }
+                Thread.sleep(1000);
+
+            } catch (Exception e) {
+                System.out.println("ERROR_FIRST" + e);
             }
-            Thread.sleep(1000);
+        }
+    }
+
+    private void test() {
+        try {
+            SerialPort[] commPorts = SerialPort.getCommPorts();
+            System.out.println("Select a port:");
+            int i = 1;
+            for(SerialPort port : commPorts) {
+                System.out.println(i++ + ". " + port.getSystemPortName());
+            }
+            Scanner s = new Scanner(System.in);
+            int chosenPort = s.nextInt();
+
+            SerialPort port = commPorts[chosenPort - 1];
+            if(port.openPort()) {
+                System.out.println("Successfully opened the port.");
+            } else {
+                System.out.println("Unable to open the port.");
+                return;
+            }
+            port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+
+            Scanner data = new Scanner(port.getInputStream());
+            System.out.println(data.next());
+            while (true) {
+                System.out.println(data.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR_SECOND : " + e);
         }
     }
 }
